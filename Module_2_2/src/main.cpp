6ab4c280517f761ee30fb3c419ee0b7991d99407
Volodymyr K <syndicateua@gmail.com>
2026-06-21 20:41:25 +0300

    homework_2_2 fixed (3.3V)

--- Module_2_2/src/main.cpp
+++ Module_2_2/src/main.cpp
@@ -4,19 +4,29 @@ class AppConfig
 {
 public:
   // --- Pins ---
-  static constexpr uint8_t RELAY_PIN_RECEIVER = 4;
-  static constexpr uint8_t RELAY_PIN_TRIGGER = 19;
+  static constexpr uint8_t RELAY_PIN_RECEIVER = 16;
+  static constexpr uint8_t RELAY_PIN_TRIGGER = 4;
 
   // --- System settings ---
   static constexpr uint32_t SERIAL_BAUD_RATE = 115200;
-  static constexpr uint32_t STARTUP_DELAY_MS = 1e3;
+  static constexpr uint32_t STARTUP_DELAY_MS = 3e3;
 
   // ------------
-  static constexpr float AMOUNT_OF_MEASURMENTS = 10;
-  static constexpr uint32_t MEASURMENTS_DELAY = 1e3;
+  static constexpr uint32_t AMOUNT_OF_MEASURMENTS = 10;
+  static constexpr uint32_t MEASURMENTS_DELAY = 200;
+  static constexpr uint32_t RELAY_STARTUP_MS = 500;
 };
 
-void ARDUINO_ISR_ATTR relayTriggered();
+void IRAM_ATTR relayTriggered();
+
+volatile uint32_t numOfRelayTrigger = 0;
+volatile uint32_t measurmentTs = 0;
+volatile uint32_t combinedTime = 0;
+volatile bool measurmentDone = 0;
+
+uint32_t measur_delay_ts = 0;
+uint16_t i = 0;
+bool currentRelayTriggerState = false;
 
 void setup()
 {
@@ -25,13 +35,12 @@ void setup()
 
   pinMode(AppConfig::RELAY_PIN_RECEIVER, INPUT_PULLUP);
   pinMode(AppConfig::RELAY_PIN_TRIGGER, OUTPUT);
+  digitalWrite(AppConfig::RELAY_PIN_TRIGGER, currentRelayTriggerState);
+
+  delay(AppConfig::RELAY_STARTUP_MS);
+
   attachInterrupt(digitalPinToInterrupt(AppConfig::RELAY_PIN_RECEIVER), relayTriggered, FALLING);
 }
-
-uint32_t measur_delay_ts = 0;
-uint32_t measurment_ts = 0;
-volatile float combinedTime = 0;
-uint16_t i = 0;
 
 void loop()
 {
@@ -40,27 +49,41 @@ void loop()
   if (i == AppConfig::AMOUNT_OF_MEASURMENTS)
   {
     i++;
-    const float result = combinedTime / AppConfig::AMOUNT_OF_MEASURMENTS;
+    float result = (combinedTime / 1000.0) / AppConfig::AMOUNT_OF_MEASURMENTS;
+    Serial.println("======================");
     Serial.print("Avg time of ");
     Serial.print(AppConfig::AMOUNT_OF_MEASURMENTS);
-    Serial.print("measurments: ");
-    Serial.println(result);
+    Serial.print(" measurments: ");
+    Serial.println(result, 3);
+    Serial.println("======================");
+    Serial.print("Num of triggers: ");
+    Serial.println(numOfRelayTrigger);
   }
-  if (current_ts - measur_delay_ts > AppConfig::MEASURMENTS_DELAY && i < AppConfig::AMOUNT_OF_MEASURMENTS)
+
+  if (i < AppConfig::AMOUNT_OF_MEASURMENTS)
   {
     measur_delay_ts = millis();
+    i++;
     Serial.print("Num of measurment:");
     Serial.println(i);
-    i++;
 
+    digitalWrite(AppConfig::RELAY_PIN_TRIGGER, LOW);
+    delay(AppConfig::MEASURMENTS_DELAY);
+
+    currentRelayTriggerState = !currentRelayTriggerState;
+    measurmentDone = false;
+    measurmentTs = micros();
     digitalWrite(AppConfig::RELAY_PIN_TRIGGER, HIGH);
-    measurment_ts = millis();
+    delay(AppConfig::MEASURMENTS_DELAY);
   }
 }
 
-void ARDUINO_ISR_ATTR relayTriggered()
+void IRAM_ATTR relayTriggered()
 {
-  digitalWrite(AppConfig::RELAY_PIN_TRIGGER, LOW);
-  uint32_t current_ts = millis();
-  combinedTime += (current_ts - measurment_ts);
+  if (!measurmentDone)
+  {
+    combinedTime = combinedTime + (micros() - measurmentTs);
+    numOfRelayTrigger++;
+    measurmentDone = true;
+  }
 }
